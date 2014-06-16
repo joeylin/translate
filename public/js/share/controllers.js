b62c9dafc5df0ab5ffc3c32c1112ad80a2783019
joeylin <331547274@qq.com>
2014-06-16 21:58:48 +0800

    update

--- public/js/share/controllers.js
+++ public/js/share/controllers.js
@@ -38,6 +38,98 @@ controller('indexCtrl', ['app', '$scope', '$rootScope', '$location', '$http',
             params.page = $scope.pager.current;
             getTrends();
         };
+        $scope.vm.toggleLike = function(share) {
+            var url;
+            if (share.liked) {
+                url = '/api/share/unlike';
+                $http.post(url, {
+                    shareId: share._id
+                }).success(function(data) {
+                    share.liked = false;
+                    share.likes -= 1;
+                });
+            } else {
+                url = '/api/share/like';
+                $http.post(url, {
+                    shareId: share._id
+                }).success(function(data) {
+                    share.liked = true;
+                    share.likes += 1;
+                });
+            }
+        };
+        $scope.vm.toggleComment = function(share) {
+            share.isShowComment = !share.isShowComment;
+            if (share.isShowComment) {
+                var params = {
+                    shareId: share._id
+                };
+                var url = '/api/share/comments';
+                $http.get(url, {
+                    params: params
+                }).success(function(data) {
+                    share.comments = data.comments;
+                });
+            }
+        };
+        $scope.vm.submitComment = function(share) {
+            if (share.newComment === '') {
+                return false;
+            }
+            var url = '/api/share/comments/add';
+            $http.post(url, {
+                shareId: share._id,
+                content: share.newComment,
+                replyTo: share.replyTo
+            }).success(function(data) {
+                var comment = {
+                    user: app.user,
+                    content: share.newComment,
+                    replyTo: share.replyTo,
+                    _id: data.content._id,
+                    date: data.content.createAt
+                };
+                share.comments.unshift(comment);
+                share.newComment = '';
+            });
+        };
+        $scope.vm.delete = function(comment, share) {
+            var index = share.comments.indexOf(comment);
+            var url = '/api/share/comments/delete';
+            $http.post(url, {
+                shareId: share._id,
+                commentIndex: index
+            }).success(function(data) {
+                share.comments.splice(index, 1);
+            });
+        };
+        $scope.vm.reply = function(comment) {
+            comment.isShowReply = !comment.isShowReply;
+            if (comment.isShowReply) {
+                comment.newComment = 'reply to ' + comment.user.name + ' : ';
+            }
+        };
+        $scope.vm.submitInlineComment = function(comment, share) {
+            if (comment.newComment === '') {
+                return false;
+            }
+            var url = '/api/share/comments/add';
+            $http.post(url, {
+                shareId: share._id,
+                content: comment.newComment,
+                replyTo: comment.user._id
+            }).success(function(data) {
+                var result = {
+                    user: app.user,
+                    content: comment.newComment,
+                    replyTo: comment.user._id,
+                    _id: data.content._id,
+                    date: data.content.createAt
+                };
+                share.comments.unshift(result);
+                comment.isShowReply = false;
+            });
+        };
 
         // init
         getTrends();
